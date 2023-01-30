84a581e3c1525ce1df5681b79ee91c6175b3d051
DESKTOP-5AFE429\Isidro Camacho <isidroswicth@gmail.com>
2023-01-30 12:23:36 +0100

    30-1-2023

---
+++ propiedadesDocuments/src/main/webapp/script/script.js
@@ -0,0 +1,17 @@
+function eje1(color){
+	 document.bgColor = color;
+}
+
+function titulo(texto){
+	    document.title = texto;
+}
+
+function dominio(){
+		let dominio = document.domain;
+		document.write("el dominio es "+dominio)
+}
+
+function url(){
+		let url = document.URL;
+		document.write("la ubicacion del documento es "+url)
+}
